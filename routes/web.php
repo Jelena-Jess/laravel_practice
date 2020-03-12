2d3a0a3e0105923b798b77d294fc414d14392b84
Jelena Petkov <petkovjelena@gmail.com>
2020-03-12 14:15:59 +0100

    Last init

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
